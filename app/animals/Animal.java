d5707d8145432244ed7c1e0a0ca6642b1f4a5f7f
Vladislav <bogrov1999@bk.ru>
2025-04-05 14:27:27 +0300

    create abstract class animal

---
+++ app/animals/Animal.java
@@ -0,0 +1,56 @@
+package app.animals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Command 
+{
+    private String name;
+
+    public Command(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+abstract class Animal
+{
+    private String name;
+    private ArrayList<Command> commands;
+    private String birthDate;
+
+    Animal(String name,  String birhDate)
+    {
+        this.name = name;
+        this.commands = new ArrayList<>();
+        this.birthDate = birhDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Command> getCommands() {
+        return commands;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void learnNewCommand(String newCommand)
+    {
+        Command command = new Command(newCommand);
+        this.commands.add(command);
+    }
+
+    public void showCommands() {
+        System.out.println("Commands for " + name + ":");
+        for (Command command : commands) {
+            System.out.println("- " + command.getName());
+        }
+    }
+}
